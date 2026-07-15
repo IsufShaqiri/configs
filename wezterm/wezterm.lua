c3a2695519983b3d332f978a1833b3fff3536b47
IsufShaqiri <isufshaqiri95@gmail.com>
2026-07-15 23:07:27 +0200

    Changed wezterm to use XWayland

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,27 +1,14 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
+local config = {}
 
--- This will hold the configuration.
-local config = wezterm.config_builder()
-
--- This is where you actually apply your config choices
-
--- For example, changing the color scheme:
--- Use this for a zero with a line through it rather than a dot
--- when using the Fira Code font
-config.harfbuzz_features = { "zero" }
--- config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
-
+config.window_background_opacity = 0.7
+config.wayland_window_background_blur = true
+config.font_size = 11
+config.font = wezterm.font("FiraCode Nerd Font")
+config.enable_wayland = false
 config.color_scheme = "rose-pine"
-config.font = wezterm.font_with_fallback({
-	"VictorMono Nerd Font",
-	"FiraCode Nerd Font",
-})
-
-config.automatically_reload_config = true
-config.window_background_opacity = 0.97
+config.use_fancy_tab_bar = false
+config.hide_tab_if_only_one_tab = true
 config.enable_tab_bar = false
-config.font = wezterm.font("JetBrains Mono", { weight = "Regular" })
-config.font_size = 11.0
--- and finally, return the configuration to wezterm
+
 return config
